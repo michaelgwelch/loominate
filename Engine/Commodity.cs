546c2e16369ac353418cb2f69608b734095ce8ce
michaelgwelch <michaelgwelch@970aeab5-a319-0410-a2ea-672cb6e556cf>
2006-07-31 01:00:05 +0000

    Added xml attributes to Commodity.cs. Switched from using NUnit define to TEST

--- Engine/Commodity.cs
+++ Engine/Commodity.cs
@@ -16,115 +16,155 @@
     You should have received a copy of the GNU General Public License
     along with Loominate; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*******************************************************************************/
-using System;
+ *******************************************************************************/
 
 namespace Loominate.Engine
 {
-    
-    public class Commodity
-    {
-        private string fullName;
-        private string nameSpace;
-        private string mnemonic;
-        private string cusip;
-        private string uniqueName;
-        private int fraction;
-        
-        public Commodity(Object book, string fullName, string nameSpace,
-            string mnemonic, string cusip, int fraction)
-        {
-            this.fullName = fullName;
-            this.nameSpace = nameSpace;
-            this.mnemonic = mnemonic;
-            this.cusip = cusip;
-            this.uniqueName = nameSpace + "::" + mnemonic;
-            this.fraction = fraction;
-        }
-        
-        public string FullName
-        {
-            get
-            {
-                return fullName;
-            }
-            set
-            {
-                fullName = value;
-            }
-        }
-        
-        public string Namespace
-        {
-            get
-            {
-                return nameSpace;
-            }
-            set
-            {
-                nameSpace = value;
-            }
-        }
-        
-        public string Mnemonic
-        {
-            get
-            {
-                return mnemonic;
-            }
-            set
-            {
-                mnemonic = value;
-            }
-        }
-        
-        public string Cusip
-        {
-            get
-            {
-                return cusip;
-            }
-            set
-            {
-                cusip = value;
-            }
-        }
-        
-        public string UniqueName
-        {
-            get
-            {
-                return uniqueName;
-            }
-        }
-        
-        public int Fraction
-        {
-            get
-            {
-                return fraction;
-            }
-            set
-            {
-                fraction = value;
-            }
-        }
-        
-        public override bool Equals(object other)
-        {
-            Commodity c = other as Commodity;           
-            if (c == null) return false;
-            
-            return (this.nameSpace == c.nameSpace
-                       && this.mnemonic == c.mnemonic);
-        }
-        
-        public override int GetHashCode()
-        {
-            return uniqueName.GetHashCode();
-        }
-            
-        
-    }
-    
+	using System;
+	using System.Xml;
+	using System.Xml.Schema;
+	using System.Xml.Serialization;
+	
+	[XmlType(Namespace="http://www.gnucash.org/XML/gnc")]
+	[XmlRoot(Namespace="http://www.gnucash.org/XML/gnc", ElementName="commodity")]
+	public class Commodity
+	{
+		private string fullName;
+		private string nameSpace;
+		private string mnemonic;
+		private string cusip;
+		private string uniqueName;
+		private int fraction;
+		const string version = "2.0.0";
+		const string cmdtyNs = "http://www.gnucash.org/XML/cmdty";
+		public Commodity() {}
+		
+		public Commodity(Object book, string fullName, string nameSpace,
+		                 string mnemonic, string cusip, int fraction)
+		{
+			this.fullName = fullName;
+			this.nameSpace = nameSpace;
+			this.mnemonic = mnemonic;
+			this.cusip = cusip;
+			this.uniqueName = nameSpace + "::" + mnemonic;
+			this.fraction = fraction;
+		}
+		
+		[XmlAttribute("version", Namespace=cmdtyNs)]
+		public String Version
+		{
+			get
+			{
+				return version;
+			}
+			set
+			{
+			
+			}
+		}
+		
+		[XmlElement("name", Namespace=cmdtyNs)]
+		public string FullName
+		{
+			get
+			{
+				return fullName;
+			}
+			set
+			{
+				fullName = value;
+			}
+		}
+		
+		[XmlElement("space", Namespace=cmdtyNs)]
+		public string Namespace
+		{
+			get
+			{
+				return nameSpace;
+			}
+			set
+			{
+				nameSpace = value;
+			}
+		}
+
+		[XmlElement("id", Namespace=cmdtyNs)]
+		public string Mnemonic
+		{
+			get
+			{
+				return mnemonic;
+			}
+			set
+			{
+				mnemonic = value;
+			}
+		}
+		
+		[XmlElement("xcode", Namespace=cmdtyNs)]
+		public string Cusip
+		{
+			get
+			{
+				return cusip;
+			}
+			set
+			{
+				cusip = value;
+			}
+		}
+		
+		public string UniqueName
+		{
+			get
+			{
+				return uniqueName;
+			}
+		}
+		
+		[XmlElement("fraction", Namespace=cmdtyNs)]
+		public int Fraction
+		{
+			get
+			{
+				return fraction;
+			}
+			set
+			{
+				fraction = value;
+			}
+		}
+		
+		public override bool Equals(object other)
+		{
+			Commodity c = other as Commodity;
+			if (c == null) return false;
+			
+			return (this.nameSpace == c.nameSpace
+			        && this.mnemonic == c.mnemonic);
+		}
+		
+		public override int GetHashCode()
+		{
+			return uniqueName.GetHashCode();
+		}
+	
+//		public void ReadXml(XmlReader reader)
+//		{
+//
+//		
+//			for(reader.MoveToContent(); reader.IsStartElement(); reader.MoveToContent())
+//			{
+//				if (reader.IsStartElement("cmdty:space")) nameSpace=reader.ReadElementString();
+//				if (reader.IsStartElement("cmdty:name")) fullName=reader.ReadElementString();
+//				if (reader.IsStartElement("cmdty:fraction")) 
+//				else reader.Skip();
+//			}
+//		
+//		}
+		
+
+	}
+	
 }
--- Engine/Test/TestCommodity.cs
+++ Engine/Test/TestCommodity.cs
@@ -17,7 +17,7 @@
     along with Loominate; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 *******************************************************************************/
-#if NUNIT
+#if TEST
 using System;
 using NUnit.Framework;
 
