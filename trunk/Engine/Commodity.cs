8bdc9f45cdff2a5885a1869488154ac41ea311ed
michaelgwelch <michaelgwelch@970aeab5-a319-0410-a2ea-672cb6e556cf>
2006-08-01 01:04:11 +0000

    converts tabs to spaces for like the 5th time.

--- trunk/Engine/Commodity.cs
+++ trunk/Engine/Commodity.cs
@@ -20,151 +20,151 @@
 
 namespace Loominate.Engine
 {
-	using System;
-	using System.Xml;
-	using System.Xml.Schema;
-	using System.Xml.Serialization;
-	
-	[XmlType(Namespace="http://www.gnucash.org/XML/gnc")]
-	[XmlRoot(Namespace="http://www.gnucash.org/XML/gnc", ElementName="commodity")]
-	public class Commodity
-	{
-		private string fullName;
-		private string nameSpace;
-		private string mnemonic;
-		private string cusip;
-		private string uniqueName;
-		private int fraction;
-		const string version = "2.0.0";
-		const string cmdtyNs = "http://www.gnucash.org/XML/cmdty";
-		public Commodity() {}
-		
-		public Commodity(Object book, string fullName, string nameSpace,
-		                 string mnemonic, string cusip, int fraction)
-		{
-			this.fullName = fullName;
-			this.nameSpace = nameSpace;
-			this.mnemonic = mnemonic;
-			this.cusip = cusip;
-			this.uniqueName = nameSpace + "::" + mnemonic;
-			this.fraction = fraction;
-		}
-		
-		[XmlAttribute("version", Namespace=cmdtyNs)]
-		public String Version
-		{
-			get
-			{
-				return version;
-			}
-			set
-			{
-			
-			}
-		}
-		
-		[XmlElement("name", Namespace=cmdtyNs)]
-		public string FullName
-		{
-			get
-			{
-				return fullName;
-			}
-			set
-			{
-				fullName = value;
-			}
-		}
-		
-		[XmlElement("space", Namespace=cmdtyNs)]
-		public string Namespace
-		{
-			get
-			{
-				return nameSpace;
-			}
-			set
-			{
-				nameSpace = value;
-			}
-		}
+    using System;
+    using System.Xml;
+    using System.Xml.Schema;
+    using System.Xml.Serialization;
 
-		[XmlElement("id", Namespace=cmdtyNs)]
-		public string Mnemonic
-		{
-			get
-			{
-				return mnemonic;
-			}
-			set
-			{
-				mnemonic = value;
-			}
-		}
-		
-		[XmlElement("xcode", Namespace=cmdtyNs)]
-		public string Cusip
-		{
-			get
-			{
-				return cusip;
-			}
-			set
-			{
-				cusip = value;
-			}
-		}
-		
-		public string UniqueName
-		{
-			get
-			{
-				return uniqueName;
-			}
-		}
-		
-		[XmlElement("fraction", Namespace=cmdtyNs)]
-		public int Fraction
-		{
-			get
-			{
-				return fraction;
-			}
-			set
-			{
-				fraction = value;
-			}
-		}
-		
-		public override bool Equals(object other)
-		{
-			Commodity c = other as Commodity;
-			if (c == null) return false;
-			
-			return (this.nameSpace == c.nameSpace
-			        && this.mnemonic == c.mnemonic);
-		}
-		
-		public override int GetHashCode()
-		{
-			return uniqueName.GetHashCode();
-		}
-	
-//		public void ReadXml(XmlReader reader)
-//		{
-//
-//		
-//			for(reader.MoveToContent(); reader.IsStartElement(); reader.MoveToContent())
-//			{
-//				if (reader.IsStartElement("cmdty:space")) nameSpace=reader.ReadElementString();
-//				if (reader.IsStartElement("cmdty:name")) fullName=reader.ReadElementString();
-//				if (reader.IsStartElement("cmdty:fraction")) 
-//				else reader.Skip();
-//			}
-//		
-//		}
-		
+    [XmlType(Namespace = "http://www.gnucash.org/XML/gnc")]
+    [XmlRoot(Namespace = "http://www.gnucash.org/XML/gnc", ElementName = "commodity")]
+    public class Commodity
+    {
+        private string fullName;
+        private string nameSpace;
+        private string mnemonic;
+        private string cusip;
+        private string uniqueName;
+        private int fraction;
+        const string version = "2.0.0";
+        const string cmdtyNs = "http://www.gnucash.org/XML/cmdty";
+        public Commodity() { }
 
-	}
-	
+        public Commodity(Object book, string fullName, string nameSpace,
+                         string mnemonic, string cusip, int fraction)
+        {
+            this.fullName = fullName;
+            this.nameSpace = nameSpace;
+            this.mnemonic = mnemonic;
+            this.cusip = cusip;
+            this.uniqueName = nameSpace + "::" + mnemonic;
+            this.fraction = fraction;
+        }
+
+        [XmlAttribute("version", Namespace = cmdtyNs)]
+        public String Version
+        {
+            get
+            {
+                return version;
+            }
+            set
+            {
+
+            }
+        }
+
+        [XmlElement("name", Namespace = cmdtyNs)]
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                fullName = value;
+            }
+        }
+
+        [XmlElement("space", Namespace = cmdtyNs)]
+        public string Namespace
+        {
+            get
+            {
+                return nameSpace;
+            }
+            set
+            {
+                nameSpace = value;
+            }
+        }
+
+        [XmlElement("id", Namespace = cmdtyNs)]
+        public string Mnemonic
+        {
+            get
+            {
+                return mnemonic;
+            }
+            set
+            {
+                mnemonic = value;
+            }
+        }
+
+        [XmlElement("xcode", Namespace = cmdtyNs)]
+        public string Cusip
+        {
+            get
+            {
+                return cusip;
+            }
+            set
+            {
+                cusip = value;
+            }
+        }
+
+        public string UniqueName
+        {
+            get
+            {
+                return uniqueName;
+            }
+        }
+
+        [XmlElement("fraction", Namespace = cmdtyNs)]
+        public int Fraction
+        {
+            get
+            {
+                return fraction;
+            }
+            set
+            {
+                fraction = value;
+            }
+        }
+
+        public override bool Equals(object other)
+        {
+            Commodity c = other as Commodity;
+            if (c == null) return false;
+
+            return (this.nameSpace == c.nameSpace
+                    && this.mnemonic == c.mnemonic);
+        }
+
+        public override int GetHashCode()
+        {
+            return uniqueName.GetHashCode();
+        }
+
+        //		public void ReadXml(XmlReader reader)
+        //		{
+        //
+        //		
+        //			for(reader.MoveToContent(); reader.IsStartElement(); reader.MoveToContent())
+        //			{
+        //				if (reader.IsStartElement("cmdty:space")) nameSpace=reader.ReadElementString();
+        //				if (reader.IsStartElement("cmdty:name")) fullName=reader.ReadElementString();
+        //				if (reader.IsStartElement("cmdty:fraction")) 
+        //				else reader.Skip();
+        //			}
+        //		
+        //		}
+
+
+    }
+
 }
--- trunk/Engine/Test/GetRandom.cs
+++ trunk/Engine/Test/GetRandom.cs
@@ -23,20 +23,20 @@ namespace Loominate.Engine
 {
     using System;
     using System.Text;
-	
-	public static class GetRandom
-	{
-   
-	    private static Random rand;
-	    static string plainCharString =
-	    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-	    "abcdefghijklmnopqrstuvwxyz" +
-	    "1234567890" + " ";
-	    
-	    static string funkyCharString = ",.'\"`~!@#$%^*(){}[]/=?+-_\\|<>&\n\t";
-	    static string randomChars;
-	    
-	    static string[] types = new string[] {
+
+    public static class GetRandom
+    {
+
+        private static Random rand;
+        static string plainCharString =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        "abcdefghijklmnopqrstuvwxyz" +
+        "1234567890" + " ";
+
+        static string funkyCharString = ",.'\"`~!@#$%^*(){}[]/=?+-_\\|<>&\n\t";
+        static string randomChars;
+
+        static string[] types = new string[] {
 	        "NASDAQ",
 	        "NYSE",
 	        "EUREX",
@@ -44,77 +44,77 @@ namespace Loominate.Engine
 	        "AMEX",
 	        "CURRENCY"
 	    };
-	    
-	    public static bool RandomCharIncludeFunkyChars
-	    {
-	        set
-	        {
-	            if (value) randomChars = plainCharString + funkyCharString;
-	            else randomChars = plainCharString;
-	        }
-	    }
-	    
-	    
-	    static GetRandom()
-	    {
-	        rand = new Random();
-	    }
-	    
-	    public static bool Boolean()
-	    {
-	        return rand.Next(2) == 1;
-	    }
-	    
-	    public static char Character()
-	    {
-	        if (randomChars == null) RandomCharIncludeFunkyChars = true;
-	        
-	        return randomChars[rand.Next(randomChars.Length)];
-	        
-	    }
-	    
 
-	    
-	    public static string StringWithout(string excludeChars)
-	    {
-	        int length;
-	        // 10% of the time create a large string.
-	        if (rand.Next(10)==0) length = rand.Next(1000, 5000);
-	        else length = rand.Next(5, 20);
-	        
-	        StringBuilder bldr = new StringBuilder(length);
-	        for (int i = 0; i < length; i++)
-	        {
-	            char c;
-	            do
-	            {
-	                c = GetRandom.Character();
-	            } while (excludeChars != null && excludeChars.IndexOf(c) < 0);
-	            bldr.Append(c);
-	        }
-	        return bldr.ToString();
-	    }
-	    
-	    public static string String()
-	    {
-	        return StringWithout(null);
-	    }
-	    
-	    public static string StringInArray(string[] strings)
-	    {
-	        if (strings==null || strings.Length==0)
-	            return null;
-	        
-	        return strings[rand.Next(strings.Length)];
-	    }
-	    
-	    public static string CommodityNamespace()
-	    {
-	        return StringInArray(types);
-	    }
-	    
-	    
-	}
-	
+        public static bool RandomCharIncludeFunkyChars
+        {
+            set
+            {
+                if (value) randomChars = plainCharString + funkyCharString;
+                else randomChars = plainCharString;
+            }
+        }
+
+
+        static GetRandom()
+        {
+            rand = new Random();
+        }
+
+        public static bool Boolean()
+        {
+            return rand.Next(2) == 1;
+        }
+
+        public static char Character()
+        {
+            if (randomChars == null) RandomCharIncludeFunkyChars = true;
+
+            return randomChars[rand.Next(randomChars.Length)];
+
+        }
+
+
+
+        public static string StringWithout(string excludeChars)
+        {
+            int length;
+            // 10% of the time create a large string.
+            if (rand.Next(10) == 0) length = rand.Next(1000, 5000);
+            else length = rand.Next(5, 20);
+
+            StringBuilder bldr = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+            {
+                char c;
+                do
+                {
+                    c = GetRandom.Character();
+                } while (excludeChars != null && excludeChars.IndexOf(c) < 0);
+                bldr.Append(c);
+            }
+            return bldr.ToString();
+        }
+
+        public static string String()
+        {
+            return StringWithout(null);
+        }
+
+        public static string StringInArray(string[] strings)
+        {
+            if (strings == null || strings.Length == 0)
+                return null;
+
+            return strings[rand.Next(strings.Length)];
+        }
+
+        public static string CommodityNamespace()
+        {
+            return StringInArray(types);
+        }
+
+
+    }
+
 }
 #endif
