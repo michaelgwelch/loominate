59c1a7ad554cb04f5d5131949d596decbdbe44a5
josh.edler <josh.edler@970aeab5-a319-0410-a2ea-672cb6e556cf>
2006-08-02 17:52:16 +0000

    Finally, a Register I can work with.  Eye-candy only for now, no actual values can be displayed.

--- Gooey/MainForm.Designer.cs
+++ Gooey/MainForm.Designer.cs
@@ -1,12 +1,24 @@
-﻿/*
- * Created by SharpDevelop.
- * User: cedlerjo
- * Date: 7/28/2006
- * Time: 9:56 AM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-namespace Gooey
+﻿/*******************************************************************************
+    Copyright 2006 Josh Edler
+    
+    This file is part of Loominate.
+
+    Loominate is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Loominate is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Loominate; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*******************************************************************************/
+
+namespace Loominate.Gooey
 {
 	partial class MainForm : System.Windows.Forms.Form
 	{
@@ -36,101 +48,102 @@ namespace Gooey
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
-			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
-			this.mainTabControl = new System.Windows.Forms.TabControl();
-			this.overviewTab = new System.Windows.Forms.TabPage();
-			this.splitContainer = new System.Windows.Forms.SplitContainer();
-			this.mainMenuStrip.SuspendLayout();
-			this.mainTabControl.SuspendLayout();
-			this.overviewTab.SuspendLayout();
-			this.splitContainer.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// mainMenuStrip
-			// 
-			this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.fileToolStripMenuItem});
-			this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
-			this.mainMenuStrip.Name = "mainMenuStrip";
-			this.mainMenuStrip.Size = new System.Drawing.Size(587, 24);
-			this.mainMenuStrip.TabIndex = 0;
-			this.mainMenuStrip.Text = "mainMenuStrip";
-			// 
-			// fileToolStripMenuItem
-			// 
-			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.exitToolStripMenuItem});
-			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
-			this.fileToolStripMenuItem.Text = "&File";
-			// 
-			// exitToolStripMenuItem
-			// 
-			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-			this.exitToolStripMenuItem.Text = "E&xit";
-			this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
-			// 
-			// mainStatusStrip
-			// 
-			this.mainStatusStrip.Location = new System.Drawing.Point(0, 371);
-			this.mainStatusStrip.Name = "mainStatusStrip";
-			this.mainStatusStrip.Size = new System.Drawing.Size(587, 22);
-			this.mainStatusStrip.TabIndex = 1;
-			this.mainStatusStrip.Text = "mainStatusStrip";
-			// 
-			// mainTabControl
-			// 
-			this.mainTabControl.Controls.Add(this.overviewTab);
-			this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.mainTabControl.Location = new System.Drawing.Point(0, 24);
-			this.mainTabControl.Name = "mainTabControl";
-			this.mainTabControl.SelectedIndex = 0;
-			this.mainTabControl.Size = new System.Drawing.Size(587, 347);
-			this.mainTabControl.TabIndex = 2;
-			// 
-			// overviewTab
-			// 
-			this.overviewTab.Controls.Add(this.splitContainer);
-			this.overviewTab.Location = new System.Drawing.Point(4, 22);
-			this.overviewTab.Name = "overviewTab";
-			this.overviewTab.Padding = new System.Windows.Forms.Padding(3);
-			this.overviewTab.Size = new System.Drawing.Size(579, 321);
-			this.overviewTab.TabIndex = 1;
-			this.overviewTab.Text = "Overview";
-			this.overviewTab.UseVisualStyleBackColor = true;
-			// 
-			// splitContainer
-			// 
-			this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer.Location = new System.Drawing.Point(3, 3);
-			this.splitContainer.Name = "splitContainer";
-			this.splitContainer.Size = new System.Drawing.Size(573, 315);
-			this.splitContainer.SplitterDistance = 191;
-			this.splitContainer.TabIndex = 0;
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(587, 393);
-			this.Controls.Add(this.mainTabControl);
-			this.Controls.Add(this.mainStatusStrip);
-			this.Controls.Add(this.mainMenuStrip);
-			this.MainMenuStrip = this.mainMenuStrip;
-			this.Name = "MainForm";
-			this.Text = "MyCash";
-			this.mainMenuStrip.ResumeLayout(false);
-			this.mainMenuStrip.PerformLayout();
-			this.mainTabControl.ResumeLayout(false);
-			this.overviewTab.ResumeLayout(false);
-			this.splitContainer.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.mainTabControl = new System.Windows.Forms.TabControl();
+            this.overviewTab = new System.Windows.Forms.TabPage();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.mainMenuStrip.SuspendLayout();
+            this.mainTabControl.SuspendLayout();
+            this.overviewTab.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(892, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "mainMenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+            // 
+            // mainStatusStrip
+            // 
+            this.mainStatusStrip.Location = new System.Drawing.Point(0, 371);
+            this.mainStatusStrip.Name = "mainStatusStrip";
+            this.mainStatusStrip.Size = new System.Drawing.Size(892, 22);
+            this.mainStatusStrip.TabIndex = 1;
+            this.mainStatusStrip.Text = "mainStatusStrip";
+            // 
+            // mainTabControl
+            // 
+            this.mainTabControl.Controls.Add(this.overviewTab);
+            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabControl.Location = new System.Drawing.Point(0, 24);
+            this.mainTabControl.Name = "mainTabControl";
+            this.mainTabControl.SelectedIndex = 0;
+            this.mainTabControl.Size = new System.Drawing.Size(892, 347);
+            this.mainTabControl.TabIndex = 2;
+            // 
+            // overviewTab
+            // 
+            this.overviewTab.Controls.Add(this.splitContainer);
+            this.overviewTab.Location = new System.Drawing.Point(4, 22);
+            this.overviewTab.Name = "overviewTab";
+            this.overviewTab.Padding = new System.Windows.Forms.Padding(3);
+            this.overviewTab.Size = new System.Drawing.Size(884, 321);
+            this.overviewTab.TabIndex = 1;
+            this.overviewTab.Text = "Overview";
+            this.overviewTab.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Size = new System.Drawing.Size(878, 315);
+            this.splitContainer.SplitterDistance = 191;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 393);
+            this.Controls.Add(this.mainTabControl);
+            this.Controls.Add(this.mainStatusStrip);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Loominate";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.mainTabControl.ResumeLayout(false);
+            this.overviewTab.ResumeLayout(false);
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 		private System.Windows.Forms.SplitContainer splitContainer;
 		private System.Windows.Forms.TabPage overviewTab;
--- Gooey/MainForm.cs
+++ Gooey/MainForm.cs
@@ -1,19 +1,30 @@
-/*
- * Created by SharpDevelop.
- * User: cedlerjo
- * Date: 7/28/2006
- * Time: 9:56 AM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+/*******************************************************************************
+    Copyright 2006 Josh Edler
+    
+    This file is part of Loominate.
+
+    Loominate is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Loominate is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Loominate; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*******************************************************************************/
 
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-using Gooey.Controls;
+using Loominate.Gooey.Controls;
 
-namespace Gooey
+namespace Loominate.Gooey
 {
 	/// <summary>
 	/// Description of MainForm.
@@ -61,19 +72,8 @@ namespace Gooey
 		#region IMainForm methods
 		public void DisplayAccount(string name)
 		{
-			TabPage page = null;
-			
-			if (mainTabControl.Controls.ContainsKey(name))
-			{
-				page = (TabPage) mainTabControl.Controls[name];
-			}
-			else
-			{
-				page = new AccountPage(name, mainTabControl);
-				mainTabControl.Controls.Add(page);
-			}
-
-			mainTabControl.SelectedIndex = page.TabIndex - 1;
+			splitContainer.Panel2.Controls.Clear();
+			splitContainer.Panel2.Controls.Add(new AccountPage(name));
 		}
 		#endregion
 	}
